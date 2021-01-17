814b59361054405d4615a9b0c87f1ee7a81991a6
burakcg <burak.c.gungor@gmail.com>
2021-01-17 15:20:12 +0300

    commit 2
    Laravel Routing Controller View Kullanımı

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>First Laravel Page</title>
+</head>
+<body>
+
+<h1>welcome Laravel</h1>
+<p>This is a paragraph.</p>
+<br>
+<a href="{{route('test', ['id' => 1, 'name' => 'burak'])}}">Test Sayfası</a>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,19 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::redirect('/anasayfa', '/home')->name('anasayfa');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->whereNumber('id')->whereAlpha('name')->name('test');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
