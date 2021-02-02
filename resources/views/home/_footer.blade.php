c9366381cd2c67435e6396d618b8d938dafc495b
burakcg <burak.c.gungor@gmail.com>
2021-02-02 21:02:31 +0300

    commit 4
     Hazır Html Web Template Uyarlaması

---
+++ resources/views/home/_footer.blade.php
@@ -0,0 +1,40 @@
+<!-- Footer section -->
+<footer class="footer-section">
+    <div class="container">
+        <div class="footer-left-pic">
+             <img src="{{asset('assets')}}/homeT/img/footer-left-pic.png" alt="">
+        </div>
+        <div class="footer-right-pic">
+             <img src="{{asset('assets')}}/homeT/img/footer-right-pic.png" alt="">
+        </div>
+        <a href="#" class="footer-logo">
+            <img src="{{asset('assets')}}/homeT/img/logo.png" alt="">
+        </a>
+        <ul class="main-menu footer-menu">
+            <li><a href="">Home</a></li>
+            <li><a href="">Games</a></li>
+            <li><a href="">Reviews</a></li>
+            <li><a href="">News</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+        <div class="footer-social d-flex justify-content-center">
+            <a href="#"><i class="fa fa-pinterest"></i></a>
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-dribbble"></i></a>
+            <a href="#"><i class="fa fa-behance"></i></a>
+        </div>
+        <div class="copyright"><a href="">Colorlib</a> 2018 @ All rights reserved</div>
+    </div>
+</footer>
+<!-- Footer section end -->
+
+
+<!--====== Javascripts & Jquery ======-->
+<script src="{{asset('assets')}}/homeT/js/jquery-3.2.1.min.js"></script>
+<script src="{{asset('assets')}}/homeT/js/bootstrap.min.js"></script>
+<script src="{{asset('assets')}}/homeT/js/jquery.slicknav.min.js"></script>
+<script src="{{asset('assets')}}/homeT/js/owl.carousel.min.js"></script>
+<script src="{{asset('assets')}}/homeT/js/jquery.sticky-sidebar.min.js"></script>
+<script src="{{asset('assets')}}/homeT/js/jquery.magnific-popup.min.js"></script>
+<script src="{{asset('assets')}}/homeT/js/main.js"></script>
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,41 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zxx">
 <head>
     <title>@yield('title')</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="BCG">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="shortcut icon"/>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{asset('assets')}}/homeT/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="{{asset('assets')}}/homeT/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="{{asset('assets')}}/homeT/css/slicknav.min.css"/>
+    <link rel="stylesheet" href="{{asset('assets')}}/homeT/css/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="{{asset('assets')}}/homeT/css/magnific-popup.css"/>
+    <link rel="stylesheet" href="{{asset('assets')}}/homeT/css/animate.css"/>
+
+    <!-- Main Stylesheets -->
+    <link rel="stylesheet" href="{{asset('assets')}}/homeT/css/style.css"/>
+
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
     @yield('css')
-    @yield('js')
+    @yield('headerjs')
 
 </head>
 <body>
 
-<div class="jumbotron text-center">
-    <h1>Header</h1>
-    <p>Resize this responsive page to see the effect!</p>
-    <a href="/home">Home</a>
-    <a href="/aboutus">About Us</a>
-    @yield('header')
-</div>
-
-<div class="container">
-    <div class="row">
-        @yield('sidebar')
-        @yield('content')
-    </div>
-</div>
-
-<div class="jumbotron text-center">
-    <h1>Footor</h1>
-    <p>sayfa altı</p>
-    @yield('footor')
-</div>
+@include('home._header')
 
 
+@section('content')
 
+@show
+
+@include('home._footer')
+@yield('footerjs')
 </body>
 </html>
